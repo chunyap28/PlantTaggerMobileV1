3e046c6b79883e5bed5292f8fb1a23923e2414c8
chunyap <chunyap@iappsasia.com>
2018-08-12 17:59:58 +0800

    Added mocked logged in page - MainPage

--- PlantTaggerV1/ViewModels/MainPageModel.cs
+++ PlantTaggerV1/ViewModels/MainPageModel.cs
@@ -5,8 +5,24 @@ namespace PlantTaggerV1.ViewModels
 {
     public class MainPageModel : ViewModelBase
     {
+        private string _gardenName;
+
         public MainPageModel()
         {
+            this._gardenName = "My Garden";
+        }
+
+        public string GardenName
+        {
+            get
+            {
+                return _gardenName;
+            }
+            set
+            {
+                _gardenName = value;
+                RaisePropertyChanged(() => GardenName);
+            }
         }
     }
 }
